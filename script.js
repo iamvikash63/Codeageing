968798f34d8ab44d5abeee27d26f33a6d108ac2a
vikashpalgit <palv2540@gmail.com>
2025-03-19 14:49:23 +0530

    making nav bar

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-document.querySelectorAll('.quick-scroll').forEach(anchor => {
+document.querySelectorAll('.quick-color-diff').forEach(anchor => {
     anchor.addEventListener('click', function(event) {
         event.preventDefault(); // Prevent default anchor behavior
   
@@ -33,10 +33,18 @@ document.querySelectorAll('.quick-scroll').forEach(anchor => {
   
         if (targetSection) {
             window.scrollTo({
-                top: targetSection.offsetTop - 50, // Adjust offset for better alignment
+                top: targetSection.offsetTop - 100, // Adjust offset for better alignment
                 behavior: 'smooth'
             });
         }
     });
   });
 
+
+  //menu 
+  const hamburger = document.querySelector(".nav-menu");
+  const menu = document.querySelector(".main-ul");
+  
+  hamburger.addEventListener("click", () => {
+      menu.classList.toggle("active");
+  });
